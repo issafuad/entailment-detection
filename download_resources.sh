cfda6cdbff2e7e0d11bf1a3a356247b06d269f0b
Fuad Issa <fuadissa@MacBook-Pro.local>
2019-01-08 21:32:45 +0000

    add resources script

---
+++ download_resources.sh
@@ -0,0 +1,7 @@
+wget https://github.com/mmihaltz/word2vec-GoogleNews-vectors/blob/master/GoogleNews-vectors-negative300.bin.gz -P data/
+wget https://nlp.stanford.edu/projects/snli/snli_1.0.zip -P data/
+apt-get install unzip
+unzip ./data/snli_1.0.zip -d ./data/
+mv ./data/snli_1.0/snli_1.0_dev.jsonl ./data/snli_1.0_dev.json
+mv ./data/snli_1.0/snli_1.0_train.jsonl ./data/snli_1.0_train.json
+mv ./data/snli_1.0/snli_1.0_test.jsonl ./data/snli_1.0_test.json
